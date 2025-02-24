23eda6cde866785e9003942b6ffa2bc784aa945e
yjgaia <yjdev88@gmail.com>
2025-02-24 21:54:52 +0900

    Add token ID generation and fetch ERC721 holder list functionality

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,14 @@
 import { BodyNode } from "@common-module/app";
-import { KaiaNFTDisplay, SupportedCollections } from "matedevdao-common";
+import { KaiaNFTDisplay, ParsingNFTDataContract, SupportedCollections, } from "matedevdao-common";
 const nft = new KaiaNFTDisplay(SupportedCollections.DogeSoundClubMates, 1n)
     .appendTo(BodyNode);
 nft.style({
     width: "300px",
     height: "300px",
 });
-for (let i = 0; i < 100; i += 1) {
+const tokenIds = [];
+for (let i = 0; i < 1000; i += 1) {
+    tokenIds.push(i);
 }
+console.log(await ParsingNFTDataContract.getERC721HolderList("0xe47e90c58f8336a2f24bcd9bcb530e2e02e1e8ae", tokenIds));
 //# sourceMappingURL=main.js.map
