fee03f4abad2bd796d6bb384f0395628d9cb3950
yjgaia <yjdev88@gmail.com>
2025-02-25 13:58:54 +0900

    Update cron job to use new Supabase Edge Function URL and authorization header

--- database/cron.sql
+++ database/cron.sql
@@ -4,9 +4,9 @@ SELECT cron.schedule(
   $$
   -- Edge Function 호출
   select net.http_post(
-      'https://ylkawitqgpeiysldwtgs.supabase.co/functions/v1/ping',
+      'https://psvyfwerxzofecujpkjt.supabase.co/functions/v1/ping',
       body := '{}'::JSONB,
-      headers := '{"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlsa2F3aXRxZ3BlaXlzbGR3dGdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4ODUzNzIsImV4cCI6MjA1MTQ2MTM3Mn0.NViN7OIedhMxJWDnBF1O3812JHRHdGhiN67ta5MWDus"}'::JSONB
+      headers := '{"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzdnlmd2VyeHpvZmVjdWpwa2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0NTc3MjAsImV4cCI6MjA1NjAzMzcyMH0.8pYFC97szdr5rmGZWm01fdhQR2MEo3N7qoOMNHjcCB4"}'::JSONB
   ) AS request_id;
   $$
 );
