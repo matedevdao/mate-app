3710a18751c509a8bd2adeacb5761202290bbdac
yj.gaia <gaiaprotocolnft@gmail.com>
2025-07-30 21:54:02 +0900

    Updates

---
+++ components/kaia-wallet/kaia-wallet.d.ts
@@ -0,0 +1,4 @@
+import { DefaultWalletOptions, Wallet } from '@rainbow-me/rainbowkit/dist/wallets/Wallet';
+export type KaiaWalletOptions = DefaultWalletOptions;
+export declare const kaiaWallet: ({ projectId, walletConnectParameters, }: KaiaWalletOptions) => Wallet;
+//# sourceMappingURL=kaia-wallet.d.ts.map
---
+++ components/kaia-wallet/utils.d.ts
@@ -0,0 +1,10 @@
+export declare function hasInjectedProvider({ flag, namespace, }: {
+    flag?: any;
+    namespace?: string;
+}): boolean;
+export declare function getInjectedConnector({ flag, namespace, target, }: {
+    flag?: any;
+    namespace?: string;
+    target?: any;
+}): any;
+//# sourceMappingURL=utils.d.ts.map
