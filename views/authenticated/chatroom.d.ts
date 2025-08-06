b96365b64e824470245dffad360a2de2ebcab972
yj.gaia <gaiaprotocolnft@gmail.com>
2025-08-06 20:26:10 +0900

    Updates

--- views/authenticated/chatroom.d.ts
+++ views/authenticated/chatroom.d.ts
@@ -1,5 +1,7 @@
 import Navigo from 'navigo';
 import { View } from '../view';
-declare function createChatRoomView(router: Navigo, roomId: string): View;
+declare function createChatRoomView(router: Navigo, roomId: string): View & {
+    scrollToBottom: () => void;
+};
 export { createChatRoomView };
 //# sourceMappingURL=chatroom.d.ts.map
