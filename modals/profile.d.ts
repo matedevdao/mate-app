93afd84bd4598a36e96e9710436031a829083ef6
yj.gaia <gaiaprotocolnft@gmail.com>
2025-10-28 10:09:43 +0900

    Updates

--- modals/profile.d.ts
+++ modals/profile.d.ts
@@ -1,4 +1,6 @@
 import Navigo from "navigo";
+export declare function createInstallAppItem(): HTMLElement | null;
+export declare function createContactUsItem(): HTMLElement;
 declare function createProfileModal(router: Navigo): HTMLElement;
 export { createProfileModal };
 //# sourceMappingURL=profile.d.ts.map
--- platform.d.ts
+++ platform.d.ts
@@ -1,3 +1,7 @@
 export declare const platform: string | null;
 export declare const isWebView: boolean;
+export declare const isIOS: boolean;
+export declare const isAndroid: boolean;
+export declare const isMobile: boolean;
+export declare const isStandalone: boolean;
 //# sourceMappingURL=platform.d.ts.map
