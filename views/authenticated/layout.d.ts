5ea7bb62e9e06bdb1bc0e172aae796c9a3c869af
yj.gaia <gaiaprotocolnft@gmail.com>
2025-10-29 11:30:56 +0900

    Updates

--- views/authenticated/layout.d.ts
+++ views/authenticated/layout.d.ts
@@ -1,5 +1,6 @@
 import Navigo from 'navigo';
 import { View } from '../view';
+export declare function changeTitle(title: string): void;
 declare function createLayoutView(router: Navigo): View;
 export { createLayoutView };
 //# sourceMappingURL=layout.d.ts.map
